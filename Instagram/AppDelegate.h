48a4f4c5df963dfa349748266fa9c87c85f9438c
Kobe Petrus <52359732+K-Pet@users.noreply.github.com>
2021-07-06 15:14:40 -0500

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Kobe Petrus on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Kobe Petrus on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
